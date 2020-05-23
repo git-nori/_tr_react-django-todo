bde63f1bda4d7ec1d5b7b78a53c0d62b07f250a9
HironoriSato <maza.hs5360@gmail.com>
2020-05-23 13:00:13 +0900

    [add]rootReducerを作成

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../features/todo/todoSlice'
+import rootReducer from './rootReducer'
 
 export default configureStore({
-  reducer: {
-    todo: todoReducer
-  },
+  reducer: rootReducer
 });
