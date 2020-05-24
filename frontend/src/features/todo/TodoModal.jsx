e350b3906267881673abda545bb198a5db59ea77
HironoriSato <maza.hs5360@gmail.com>
2020-05-24 23:15:26 +0900

    [add]TodoModal componentを実装

---
+++ frontend/src/features/todo/TodoModal.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+import { Modal, Button } from 'react-bootstrap'
+
+import TodoForm from './TodoForm'
+
+const TodoModal = ({ onCrtTodo }) => {
+  const [show, setShow] = useState(false)
+  const toggleForm = () => setShow(!show)
+
+  const onClose = () => {
+    setShow(false)
+  }
+
+  const createTodo = (form) => {
+    onCrtTodo(form)
+  }
+
+  return (
+    <>
+      <Button variant="primary" onClick={toggleForm}>Create Todo</Button>
+
+      <Modal show={show} onHide={onClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Todo Form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TodoForm onClose={onClose} createTodo={createTodo} />
+        </Modal.Body>
+      </Modal>
+    </>
+  )
+}
+
+export default TodoModal
