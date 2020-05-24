0c82c62b18378db2bf80da5e9e343f94980f0c73
HironoriSato <maza.hs5360@gmail.com>
2020-05-25 00:29:12 +0900

    [mod]不要なアプリを削除

--- todos/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
-from todos.models import Todo
-from todos.serializers import TodoSerializer
-
-
-class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
